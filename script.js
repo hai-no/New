c63b794eea088238e1737c58886961ace1a73f53
hai-no <msultanalf@gmail.com>
2024-11-20 08:58:55 +0700

    sidebar fix

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+const sidebar = document.getElementById("sidebar");
+const toggleBtn = document.getElementById("toggle-btn");
+// const jamElement = document.getElementById("jam");
+const liveTime = document.getElementById("live-time");
+
+// // Toggle sidebar
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
+
+// // update jam
+function updateLiveTime() {
+  const jakartaTime = new Date().toLocaleString("en-US", {
+    timeZone: "Asia/Jakarta",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  });
+  liveTime.textContent = `Jakarta: ${jakartaTime}`;
+}
+
+// // Refresh time every second
+setInterval(updateLiveTime, 1000);
+
+updateLiveTime();
+
 const dropdownMenus = document.querySelectorAll(".navigasi");
 
 dropdownMenus.forEach((dropdownMenu) => {
